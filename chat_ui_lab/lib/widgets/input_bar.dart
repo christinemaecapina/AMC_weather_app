7f659aad47543d51e741caa4ad3c22ac4db232cc
christinemaecapina <christinemaecapina5@gmail.com>
2026-01-20 16:43:32 +0800

    lab 2 Flutter chat application

---
+++ chat_ui_lab/lib/widgets/input_bar.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class InputBar extends StatefulWidget {
+
+  final Function(String) onSendMessage;
+
+  const InputBar({Key? key, required this.onSendMessage}) : super(key: key);
+
+  @override
+  State<InputBar> createState() => _InputBarState();
+
+}
+
+class _InputBarState extends State<InputBar> {
+  final TextEditingController _textController =TextEditingController();
+
+  @override
+  void dispose() {
+    _textController.dispose();
+    super.dispose();
+  }
+
+  void _sendMesssage() {
+     final text = _textController.text.trim();
+     if (text.isNotEmpty) {
+       widget.onSendMessage(text);
+       _textController.clear();
+     }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(8),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              controller: _textController,
+              decoration: InputDecoration(
+              hintText: 'Type your message...',
+              border: OutlineInputBorder(borderRadius: BorderRadius.circular(24)),
+              contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+            ),
+      onSubmitted: (value) =>_sendMesssage(),
+            ),
+          ),
+    SizedBox(width: 8),
+          FloatingActionButton(
+    onPressed: _sendMesssage,
+    mini: true,
+    backgroundColor: Colors.deepPurpleAccent,
+    child: Icon(Icons.send),
+    ),
+        ],
+      )
+    );
+  }
+
+}
